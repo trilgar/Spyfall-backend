2548618ea4bbec603a6fe306fbb037848a11edf5
trilgar <zaritkiy.alexey@gmail.com>
2021-06-12 12:17:44 +0300

    add player fix

--- src/main/java/com/games/spyfall/services/game/GameServiceImpl.java
+++ src/main/java/com/games/spyfall/services/game/GameServiceImpl.java
@@ -68,6 +68,10 @@ public class GameServiceImpl implements GameService {
         if (playerMap.isEmpty()) {
             setHostName(token);
         }
+        if(playerMap.containsKey(username)){
+            putPlayer(username, session);
+            return;
+        }
         if (gameReadyStatus && !playerMap.containsKey(username)) {
             log.info("GAME ALREADY STARTED. DECLINING new register " + username);
             playerMap.get(username).sendMessage(convert(new ResponseMessage(WsResponseType.ERROR, STRING_DATA_TYPE, "Game already started.")));
